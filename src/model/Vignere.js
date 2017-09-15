fb912a48924a9f2a57898ec9e89ba8a7a20c6433
Martin Peck <martinjohnpeck@gmail.com>
2017-09-15 23:55:50 +0100

    first attempt at cipher model

--- src/model/Vignere.js
+++ src/model/Vignere.js
@@ -1,5 +1,36 @@
-function vignereImpl (text, key, type) {
-  return text.trim().toUpperCase()
+const ALPHABET = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+
+function vignereImpl (text = '', key = '', type = encodeDecodeType.ENCODE) {
+  var transformedText = ''
+  var keyCounter = 0
+
+  text = text.trim().toUpperCase()
+  key = key.trim().toUpperCase()
+
+  for (const char of text) {
+    const locationChar = ALPHABET.search(char)
+
+    if (locationChar >= 0) {
+      const keyChar = key[keyCounter % key.length]
+      const locationKeyChar = ALPHABET.search(keyChar)
+
+      var transformedChar
+
+      if (type === encodeDecodeType.ENCODE) {
+        transformedChar = ALPHABET[(locationChar + locationKeyChar) % ALPHABET.length]
+      } else {
+        transformedChar = ALPHABET[(locationChar - locationKeyChar) % ALPHABET.length]
+      }
+
+      transformedText += transformedChar
+
+      keyCounter++
+    } else {
+      transformedText += char
+    }
+  }
+
+  return transformedText
 }
 
 const encodeDecodeType = {
